3c4423e78a8efaec5993a3f328b768058565990c
DinhBinhh <dinhbinht10@gmail.com>
2025-05-24 21:54:50 +0700

    feat: them eventitem va trang comment xu li khi nhan vao

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import ComEvent from '../components/Event.vue';
 import ComEventDetail from '../components/EventDetail.vue';
 import NotFound from '../components/404.vue';
-
+import PostComment from '@/components/PostCommentPage.vue'
+//http://localhost:8080/test-event dùng để test eventitem
+//http://localhost:8080/post/1/comment dùng để test post comment
 const routes = [
   {
     path: '/',
@@ -18,6 +20,12 @@ const routes = [
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: NotFound,
+  },
+  {
+    path: '/post/:postId/comment',
+    name: 'PostComment',
+    component: PostComment,
+    props: true
   },
 ];
 
