55e94ed788694e1f0ff329c5a66057c0d15abd3f
NKhangg201204 <nguyen.khangg201204@gmail.com>
2025-05-31 00:05:12 +0700

    remove test-event

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,6 @@
     <main class="flex-1 w-full text-gray-900 dark:text-white">
       <!-- Thêm route tạm để test -->
       <router-view />
-      <div class="test-container" v-if="$route.path === '/test-event'">
-        <h2>Test EventItem Component</h2>
-        <div class="event-list">
-          <EventItem title="Sự kiện tháng 6 - Đăng bài hàng tuần" :reward="{ type: 'points', value: 100 }"
-            dueDate="2024-06-30T23:59:59" :joined="true" :postId="1" />
-
-          <EventItem title="Thử thách comment tích cực" :reward="{ type: 'voucher' }" status="done" :joined="false"
-            :postId="2" />
-
-          <EventItem title="Sự kiện đang tải" :reward="{ type: 'points', value: 200 }" :loading="true" />
-        </div>
-      </div>
     </main>
     <ComFooter />
   </div>
