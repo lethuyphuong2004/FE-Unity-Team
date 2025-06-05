e0e5eb2e67dc61f65a9d8a2bd72e3002f9281e54
Duc Manh <buidiep.ducmanh@gmail.com>
2025-06-05 14:57:02 +0700

    Pull code cua Khang voi update language title

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -7,7 +7,7 @@
       <PostItem 
           :post="{
             id: 1,
-            title: 'Leave meaningful comments on 10 different posts to earn points and level up',
+            title: 'Tran Nguyen Gia Huy 221403 find a lover for rest of life',
             author: 'Đức',
             reward: { type: 'points', value: 100 },
             createdAt: new Date().toISOString(),
@@ -21,7 +21,7 @@
         <PostItem 
           :post="{   
             id: 2,
-            title: 'Leave meaningful comments on 5 different posts to earn points and level up',
+            title: 'Thien An writes an open letter to Jack and their daughter Sol.',
             author: 'Community Team',
             reward: { type: 'points', value: 50 },
             createdAt: new Date().toISOString(),
@@ -35,7 +35,7 @@
         <PostItem 
           :post="{   
             id: 3,
-            title: 'Leave meaningful comments ... to earn points and level up',
+            title: 'Ronaldo nets his 937th career goal.',
             author: 'Community Team',
             reward: { type: 'points', value: 50 },
             createdAt: new Date().toISOString(),
@@ -50,12 +50,12 @@
     <h3 class="text-xl font-bold mb-4">Join exciting events to gain experience</h3>
     <div class="event-list space-y-4">
       <EventItem
-        title="Sự kiện ở TP.HCM"
+        title="Event at Ho Chi Minh City"
         :reward="{ type: 'points', value: 100 }"
         dueDate="2024-06-30T23:59:59"
         :joined="false"
         :ticketPurchased="false"
-        location="Nhà văn hoá Thanh Niên"
+        location="Ho Chi Minh City Youth Cultural House"
         :postId="1"
       />
 
